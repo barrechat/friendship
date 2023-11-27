3bde89147c27f01fad2b80554a4f8e6b669cf06c
Abel Deltoro Hernández <ADELTOR1@upv.edu.es>
2023-11-27 14:58:44 +0100

    interfaz de ajustes hecha

--- lib/Pages/perfil.dart
+++ lib/Pages/perfil.dart
@@ -16,36 +16,131 @@ class Perfil extends StatefulWidget {
 
 class _PerfilState extends State<Perfil> {
   final supabase = SupabaseClient('https://peaoifidogwgoxzrpjft.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlYW9pZmlkb2d3Z294enJwamZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY2MDExNDcsImV4cCI6MjAxMjE3NzE0N30.xPOHo3wz93O9S0kWU9gbGofVWlFOZuA7JB9UMAMoBbA');
+  // Nombre del bucket y archivo que deseas obtener
+  final String avatar = "https://peaoifidogwgoxzrpjft.supabase.co/storage/v1/object/public/avatares/avatar.png";
+  bool notificationEnabled = true;
+  String selectedLanguage = 'Español';
+  void mostrarOpciones() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Configuraciones'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                // Contenido de la ventana emergente (ajustes, idioma, notificaciones, etc.)
+                ListTile(
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Idioma'),
+                      Text(selectedLanguage), // Muestra el idioma seleccionado
+                    ],
+                  ),
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Seleccionar idioma'),
+                          content: DropdownButton<String>(
+                            value: selectedLanguage,
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                selectedLanguage = newValue!;
+                              });
+                              Navigator.pop(context); // Cierra el diálogo al cambiar el idioma
+                            },
+                            items: <String>['Español', 'Inglés'] // Opciones de idioma
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              child: Text('Aceptar'),
+                              onPressed: () {
+                                // Acción para el idioma seleccionado
+                                // ...
+                                Navigator.pop(context); // Cierra el diálogo
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+                ListTile(
+                  title: Text('Notificaciones'),
+                  subtitle: Text('Activar/desactivar notificaciones'),
+                  trailing: Checkbox(
+                    value: notificationEnabled,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        notificationEnabled = !notificationEnabled;
+                        Navigator.pop(context);
+                        // Aquí puedes incluir la lógica para activar o desactivar las notificaciones
+                        // Por ejemplo, invocar métodos del plugin de notificaciones
+                        // Para simplicidad, aquí solo se actualiza el estado de la casilla
+                      });
+                    },
 
-  Future<String?> downloadFile() async {
-
-    final response = await supabase
-        .storage
-        .from('avatares')
-        .download('https://peaoifidogwgoxzrpjft.supabase.co/storage/v1/object/public/avatares/avatares/pngegg.png');
+                  ),
+                ),
+                ListTile(
+                  title: Text('Cerrar sesión'),
+                  onTap: () {
+                    // Acción para cerrar sesión
+                    // ...
+                    Navigator.pop(context); // Cierra el diálogo
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
+
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FutureBuilder<String?>(
-        future: downloadFile(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
-          } else if (snapshot.hasError) {
-            return Text('Error al descargar la imagen' + snapshot.error.toString());
-          } else if (snapshot.hasData) {
-            return Image.file(
-              File(snapshot.data!),
-              width: 200,
-              height: 200,
-              fit: BoxFit.cover,
-            );
-          } else {
-            return Text('No se pudo obtener la imagen');
-          }
-        },
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Perfil"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              mostrarOpciones(); // Mostrar el diálogo de configuración
+            },
+          ),
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Row(
+              children: [
+                SizedBox(width: 150),
+                Image.network(
+                  avatar,
+                  height: 100,
+                  width: 100,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
