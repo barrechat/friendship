9ce13ae982e06c146df60e5ff356013d0571d3e3
Alejandro Ferrer Fernandez <AFERFER2@upv.edu.es>
2023-12-02 14:06:06 +0100

    Reajuste de pantalla (prueba)

--- lib/Pages/recomendacion.dart
+++ lib/Pages/recomendacion.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:friendship/Class/consultas.dart';
 import 'package:friendship/Class/evento.dart';
 import 'package:friendship/Class/filtro.dart';
