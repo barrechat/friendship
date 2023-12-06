abccb97a8927ef5d7c54a56e09437a74b67e64a4
Alejandro Ferrer Fernandez <AFERFER2@upv.edu.es>
2023-12-06 19:47:27 +0100

    Tests trofeos

--- lib/Pages/perfil.dart
+++ lib/Pages/perfil.dart
@@ -20,6 +20,30 @@ class _PerfilState extends State<Perfil> {
   final String avatar = "https://peaoifidogwgoxzrpjft.supabase.co/storage/v1/object/public/avatares/avatar.png";
   bool notificationEnabled = true;
   String selectedLanguage = 'Español';
+
+  final String trofeo1Imagen = "https://peaoifidogwgoxzrpjft.supabase.co/storage/v1/object/public/avatares/trofeo.png?t=2023-11-26T17%3A45%3A04.380Z";
+  final String trofeo25Imagen = "https://peaoifidogwgoxzrpjft.supabase.co/storage/v1/object/public/avatares/trofeo25.png?t=2023-11-26T19%3A01%3A34.675Z";
+  final String trofeo50Imagen = "https://peaoifidogwgoxzrpjft.supabase.co/storage/v1/object/public/avatares/trofeo50.png?t=2023-11-26T19%3A05%3A48.647Z";
+  final String noTrofeoImagen = "https://peaoifidogwgoxzrpjft.supabase.co/storage/v1/object/public/avatares/noTrofeo.png?t=2023-11-26T18%3A17%3A57.454Z";
+
+  final int eventosUsuario = 52;
+  var trofeo1 = '';
+  var trofeo2 = '';
+  var trofeo3 = '';
+  var telefono = '';
+
+  void mostrarTrofeos(int eventosCreados){
+    if(eventosCreados >= 1){trofeo1=trofeo1Imagen;}else{trofeo1=noTrofeoImagen;}
+    if(eventosCreados >= 25){trofeo2=trofeo25Imagen;}else{trofeo2=noTrofeoImagen;}
+    if(eventosCreados >= 50){trofeo3=trofeo50Imagen;}else{trofeo3=noTrofeoImagen;}
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    mostrarTrofeos(eventosUsuario);
+  }
+
   void mostrarOpciones() {
     showDialog(
       context: context,
@@ -163,6 +187,59 @@ class _PerfilState extends State<Perfil> {
                     child: QRImage(50)
                 ),
               ],
+            ),
+            SizedBox(height: 40.0),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.network(
+                          trofeo1,
+                          height: 100,
+                          width: 100,
+                        ),
+                        SizedBox(height: 20),
+                        Text('1 evento'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.network(
+                          trofeo2,
+                          height: 100,
+                          width: 100,
+                        ),
+                        SizedBox(height: 20),
+                        Text('25 eventos'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.network(
+                          key:Key("copa-del-mundo"),
+                          trofeo3,
+                          height: 100,
+                          width: 100,
+                        ),
+                        SizedBox(height: 20),
+                        Text('50 eventos'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
